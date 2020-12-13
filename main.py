282f3643616fba48f3822c7a8fd445e6b14870ad
yeshwitha yalaverthi <yalaverthi.yeshwitha5@gmail.com>
2020-12-13 20:58:33 +0100

    edited main.py

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
-
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
+    return 'Usage;\n<Operation>?X=<input>\n'
 
 if __name__ == "__main__":
     app.run()
