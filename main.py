332aee4449fc89761652b2b073caa14ebaab0aec
yeshwitha <yeya18@student.bth.se>
2021-06-06 18:01:17 +0200

    added main.py for branch average

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?X=<input>\n'
+
+def Inputs_method():
+    try:
+        inputs = request.args.get('X', type=str)
+        inputs = inputs.split(',')
+        values = []
+        for value in inputs:
+            value = float(value)
+            values.append(value)
+        return values
+    except ValueError:
+        Error_msg = "Please enter inputs properly not in p/q form \n"
+        return Error_msg
+
+
+@app.route('/average')
+@app.route('/avg')
+@app.route('/mean')
+
+def average():
+    values = Inputs_method()
+    if not type(values) is str:
+        avrg = statistics.mean(values)
+        if avrg.is_integer():
+            return str(int(avrg)) + ' \n'
+        else:
+            return str(float((avrg))) + ' \n'
+    else:
+        return values
+
+
 
 if __name__ == "__main__":
     app.run()
