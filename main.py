e0da849d60a2f3c7b9a8fac36dc885f4b666a199
yeshwitha <yeya18@student.bth.se>
2021-06-06 17:55:33 +0200

    added main.py for branch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from fractions import Fraction
 
 app = Flask(__name__)
 
@@ -7,12 +8,26 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+@app.route('/div')
+def division():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        val_1 = Fraction(value1)
+        val_2 = Fraction(value2)
+        try:
+            result = val_1/val_2
+            return str(float(result))
+        except ZeroDivisionError as error:
+            return 'None'
 
 
 if __name__ == "__main__":
