d129d31690acac56a078938efb30bbca7697b63e
yeshwitha <yeya18@student.bth.se>
2021-06-06 18:16:27 +0200

    added main.py

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from flask import Flask, request
+import statistics
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+
+
+def yeshw():
+    try:
+        inputs = request.args.get('X', type=str)
+        inputs = inputs.split(',')
+        values = []
+        for value in inputs:
+            value = float(value)
+            values.append(value)
+        return values
+    except ValueError:
+        Error_msg = "Please enter inputs properly not in p/q form \n"
+        return Error_msg
+
+
+@app.route('/median')
+def median():
+    values = yeshw()
+    if not type(values) is str:
+        avrg = statistics.median(values)
+        if avrg.is_integer():
+            return str(int(avrg))
+        else:
+            return str(float((avrg)))
+    else:
+        return values
 
 
 if __name__ == "__main__":
